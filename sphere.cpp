f4b14c836c659c92cf863835cf7db94c9683a88d
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-11-24 14:05:50 +0100

    Ajout de scene02.yaml

--- sphere.cpp
+++ sphere.cpp
@@ -9,9 +9,9 @@
 //    Maarten Everts
 //    Jasper van de Gronde
 //
-//  This framework is inspired by and uses code of the raytracer framework of 
+//  This framework is inspired by and uses code of the raytracer framework of
 //  Bert Freudenberg that can be found at
-//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html
 //
 
 #include "sphere.h"
@@ -22,53 +22,53 @@
 
 Hit Sphere::intersect(const Ray &ray)
 {
-    /****************************************************
-    * RT1.1: INTERSECTION CALCULATION
-    *
-    * Given: ray, position, r
-    * Sought: intersects? if true: *t
-    * 
-    * Insert calculation of ray/sphere intersection here. 
-    *
-    * You have the sphere's center (C) and radius (r) as well as
-    * the ray's origin (ray.O) and direction (ray.D).
-    *
-    * If the ray does not intersect the sphere, return Hit::NO_HIT().
-    * Otherwise, return an instance of Hit() with the distance of the
-    * intersection point from the ray origin as t and the normal ad N (see example).
-    ****************************************************/
+  /****************************************************
+   * RT1.1: INTERSECTION CALCULATION
+   *
+   * Given: ray, position, r
+   * Sought: intersects? if true: *t
+   *
+   * Insert calculation of ray/sphere intersection here.
+   *
+   * You have the sphere's center (C) and radius (r) as well as
+   * the ray's origin (ray.O) and direction (ray.D).
+   *
+   * If the ray does not intersect the sphere, return Hit::NO_HIT().
+   * Otherwise, return an instance of Hit() with the distance of the
+   * intersection point from the ray origin as t and the normal ad N (see example).
+   ****************************************************/
 
-    // place holder for actual intersection calculation
+  // place holder for actual intersection calculation
 
-    
-    Vector OC = (position - ray.O);
-    //Distance between C and ray
-    double distance = (OC - (  (OC.dot(ray.D)/ray.D.dot(ray.D)) *ray.D ) ).length();
-    
-    if (distance > r) {
-        return Hit::NO_HIT();
-    }
-	//Distance of the projection of C on the ray from the ray origin
-	double tco = sqrt(OC.dot(OC) - distance * distance);
-	//Distance of the projection of C on the ray from the intersection point
-	double tcp = sqrt(r * r - distance * distance);
-	//Distance of the intersection point from the ray origin
-	double t = tco - tcp;
 
-    /****************************************************
-    * RT1.2: NORMAL CALCULATION
-    *
-    * Given: t, C, r
-    * Sought: N
-    * 
-    * Insert calculation of the sphere's normal at the intersection point.
-    ****************************************************/
-	
-	//Intersection point P = O + OP, (OP = t * ray.D)
-	Vector P = ray.O + t * ray.D;
-	//Vector CP = N
-	Vector N = P - position;
-	N.normalize();
+  Vector OC = (position - ray.O);
+  //Distance between C and ray
+  double distance = (OC - (  (OC.dot(ray.D)/ray.D.dot(ray.D)) *ray.D ) ).length();
 
-    return Hit(t,N);
+  if (distance > r) {
+    return Hit::NO_HIT();
+  }
+  //Distance of the projection of C on the ray from the ray origin
+  double tco = sqrt(OC.dot(OC) - distance * distance);
+  //Distance of the projection of C on the ray from the intersection point
+  double tcp = sqrt(r * r - distance * distance);
+  //Distance of the intersection point from the ray origin
+  double t = tco - tcp;
+
+  /****************************************************
+   * RT1.2: NORMAL CALCULATION
+   *
+   * Given: t, C, r
+   * Sought: N
+   *
+   * Insert calculation of the sphere's normal at the intersection point.
+   ****************************************************/
+
+  //Intersection point P = O + OP, (OP = t * ray.D)
+  Vector P = ray.O + t * ray.D;
+  //Vector CP = N
+  Vector N = P - position;
+  N.normalize();
+
+  return Hit(t,N);
 }
